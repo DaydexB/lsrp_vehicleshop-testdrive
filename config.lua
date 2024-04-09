dc57e642ef96d82784341d25491e6d4206c6bd9c
Solix <81125185+DaydexB@users.noreply.github.com>
2024-04-09 05:24:24 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,11 @@
 --- GO TO lsrp_vehicleshop AND ADD THIS INTO YOUR shared/config.lua do not replace it
-
-
 Config.TestDriveTime = 15 -- 15 seconds
-Config.TestDriveLocation = vec3(-33.0755, -1095.3330, 27.2744) -- Location to spawn after selecting test drive
-Config.TestDriveVehicleHeading = 250.10 -- where the vehicle is heading
+
+
+Config.TestDriveSpawnVehicleLocation =  vector3(-972.8182, -3354.9409, 13.9444) -- Location to spawn after selecting Test Drive
+Config.TestDriveSpawnHeading = 50.20
+
+
+
+Config.TestDriveFinishedLocation = vec3(-33.0755, -1095.3330, 27.2744) -- Location to spawn after Finsishing Test Drive
+Config.TestDriveFinishedHeading = 250.10 
