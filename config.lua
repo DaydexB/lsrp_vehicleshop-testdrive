b1ba4d163a1eb99c7e95492c6d059b8401e54a34
Solix <81125185+DaydexB@users.noreply.github.com>
2024-04-09 05:13:01 -0500

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,6 @@
+--- GO TO lsrp_vehicleshop AND ADD THIS INTO YOUR shared/config.lua do not replace it
+
+
+Config.TestDriveTime = 15 -- 15 seconds
+Config.TestDriveLocation = vec3(-33.0755, -1095.3330, 27.2744) -- Location to spawn after selecting test drive
+Config.TestDriveVehicleHeading = 250.10 -- where the vehicle is heading
---
+++ testdrive.lua
@@ -0,0 +1,54 @@
+local mapper = require('shared.modules.configMapper')
+local testdrive = {}
+
+local function teleportPlayerIntoVehicle(vehicle)
+    local playerPed = PlayerPedId()
+    TaskWarpPedIntoVehicle(playerPed, vehicle, -1)
+end
+
+testdrive.initiate = function(shopIndex, selected, scrollIndex, testOptions)
+    local CFG_VEH_DATA = mapper.getVehicle(shopIndex, selected, scrollIndex)
+    local CFG_SHOP_DATA = mapper.getShop(shopIndex)
+    if lib.alertDialog({
+        header = ('%s - %s'):format(CFG_SHOP_DATA.SHOP_LABEL, locale('testdrive')),
+        content = ('You are about to test drive %s at %s'):format(CFG_VEH_DATA.label, testOptions[1]),
+        centered = true,
+        cancel = true
+    }) == 'confirm' then
+        local vehicleModel = CFG_VEH_DATA.VEHICLE_MODEL
+        local coords = vector3(-972.8182, -3354.9409, 13.9444) 
+        local heading = 50.20 
+        local vehicle = CreateVehicle(vehicleModel, coords.x, coords.y, coords.z, heading, true, false)
+        if vehicle ~= 0 then
+            teleportPlayerIntoVehicle(vehicle)
+            testdrive.startTestDrive(vehicle)
+            return true
+        else
+            ESX.ShowNotification('Failed to create vehicle!')
+            return false
+        end
+    else
+        return false
+    end
+end
+
+function testdrive.startTestDrive(vehicle)
+    local ped = PlayerPedId()
+    SetEntityVisible(ped, true)
+    local coords = Config.TestDriveLocation
+    local heading = Config.TestDriveVehicleHeading
+    local duration = Config.TestDriveTime
+
+    Citizen.CreateThread(function()
+        while duration > 0 do
+            Citizen.Wait(100)
+            duration = duration - 0.1 
+        end
+        if duration <= 0 then
+            DeleteEntity(vehicle)
+            SetEntityCoords(ped, coords.x, coords.y, coords.z, true, false, false, false)
+        end
+    end)
+end
+
+return testdrive
